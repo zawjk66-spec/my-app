5394eae2f3b0dad5305ab4ba6d76f810a8468392
zawjk66-spec <zawjk66@gmail.com>
2025-12-17 15:50:38 +0100

    service-worker.js
    Implement service worker for caching and fetch handling.

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", e => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
